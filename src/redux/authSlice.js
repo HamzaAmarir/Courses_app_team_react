9ef3c49013cb283d6c8b794ae040c8dbaa7af189
HamzaAmarir <2000090800482@ofppt-edu.ma>
2024-10-18 10:19:25 +0100

    new branch test

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,14 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-// Async thunk for login
 export const login = createAsyncThunk('auth/login', async ({ username, password }) => {
-  const response = await fetch('http://localhost:5500/users'); // Adjusted to get user data
+  const response = await fetch('http://localhost:5500/users'); 
   if (!response.ok) {
     throw new Error('Login failed');
   }
 
   const users = await response.json();
-  // Find the user by username and password
   const user = users.find(user => user.username === username && user.password === password);
 
   if (!user) {
@@ -18,31 +16,28 @@ export const login = createAsyncThunk('auth/login', async ({ username, password 
   return { user: user, role: user.role }; 
 });
 
-// Creating the slice
 const authSlice = createSlice({
   name: 'auth',
   initialState: { user: null, role: null, error: null },
   reducers: {
-    // Logout action
     logout: (state) => {
       state.user = null;
       state.role = null;
-      state.error = null; // Clear error on logout
+      state.error = null; 
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(login.fulfilled, (state, action) => {
-        state.user = action.payload.user; // Set the user
-        state.role = action.payload.role; // Set the user role
-        state.error = null; // Clear any existing error
+        state.user = action.payload.user; 
+        state.role = action.payload.role; 
+        state.error = null;
       })
       .addCase(login.rejected, (state, action) => {
-        state.error = action.error.message; // Set error message
+        state.error = action.error.message; 
       });
   },
 });
 
-// Export the logout action and reducer
 export const { logout } = authSlice.actions;
 export default authSlice.reducer;
